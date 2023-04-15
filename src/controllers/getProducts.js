dba1accbcd7962917acbe2f1fcbc63cc29a1cb3f
Facundo Monllor <facundomonllor@gmail.com>
2023-04-15 03:08:57 -0300

    prueba commit

--- src/controllers/getProducts.js
+++ src/controllers/getProducts.js
@@ -3,10 +3,10 @@ import products from "../data/data.json" assert { type: "json" };
 
 export const getProducts = async (req,res) => {
     try{
-        const {namee} = req.params
-        console.log(namee)
-        if(namee){
-          const productFiltrados = products.filter((p) => p.name === namee)
+        const {name} = req.params
+        console.log(name)
+        if(name){
+          const productFiltrados = products.filter((p) => p.name === name)
           return res.status(200).send(productFiltrados)
         }
         res.status(201).send(products)
--- src/routes/subRoutes/products.routes.js
+++ src/routes/subRoutes/products.routes.js
@@ -8,7 +8,7 @@ const productsRouter = Router();
 // }
 
 productsRouter.get('/', getProducts)
-productsRouter.get('/:namee', getProducts)
+productsRouter.get('/:name', getProducts)
 // productsRouter.post('/', compaaa)
 // productsRouter.put('/', compaaa)
 // productsRouter.delete('/', compaaa)
